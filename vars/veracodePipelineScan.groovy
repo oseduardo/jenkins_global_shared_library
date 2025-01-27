7c005e01e45baaf443359d1586e588dc61b562ed
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-01-26 22:34:49 -0500

    hola mundo

--- vars/veracodePipelineScan.groovy
+++ vars/veracodePipelineScan.groovy
@@ -1,5 +1,3 @@
-def call(Map config = [:]) {
-    echo "[INFO] Preparing Veracode SAST Pipeline Scan Execution..."
-    echo "[INFO] VID: ${config.VID}"
-    echo "[INFO] VKEY: ${config.VKEY}"
+def call() {
+    echo "Hola Mundo"
 }
