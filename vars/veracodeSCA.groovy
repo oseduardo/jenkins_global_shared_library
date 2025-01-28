61c4f5d318b6a56cdee8b1d039f5986314bd7cf6
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-01-27 20:37:59 -0500

    veracodeSCA

--- vars/veracodeSCA.groovy
+++ vars/veracodeSCA.groovy
@@ -1,3 +1,6 @@
-def call() {
+def call(String SRCCLR_API_TOKEN, String SRCCLR_WORKSPACE_SLUG) {
     echo "[INFO] Veracode SCA - Agent-Based Scan"
+    sh "export SRCCLR_API_TOKEN=${SRCCLR_API_TOKEN}"
+    sh "export SRCCLR_WORKSPACE_SLUG=${SRCCLR_WORKSPACE_SLUG}"
+    curl -sSL https://download.sourceclear.com/ci.sh | bash -s -- scan --skip-compile --update-advisor --allow-dirty
 }
