0673fa631dcac65619e3bcbf54bfd627e32ed92e
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-01-27 20:56:30 -0500

    zsdvz

--- vars/veracodeSCA.groovy
+++ vars/veracodeSCA.groovy
@@ -3,7 +3,6 @@ def call(String SRCCLR_API_TOKEN, String SRCCLR_WORKSPACE_SLUG) {
     sh "ls -al"
     dir('./app') {
         sh "export SRCCLR_API_TOKEN=${SRCCLR_API_TOKEN}"
-        sh "export SRCCLR_WORKSPACE_SLUG=${SRCCLR_WORKSPACE_SLUG}"
-        sh "curl -sSL https://download.sourceclear.com/ci.sh | sh -s -- scan --skip-compile --update-advisor --allow-dirty"
+        sh "curl -sSL https://download.sourceclear.com/ci.sh | sh -s -- scan --skip-compile --update-advisor --allow-dirty --ws=${SRCCLR_WORKSPACE_SLUG}"
     }
 }
