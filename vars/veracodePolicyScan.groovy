988767e30a678cdea85fec71b1e62684cb51331e
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-08 12:03:18 -0400

    Incluyendo parametro app profile name

--- vars/veracodePolicyScan.groovy
+++ vars/veracodePolicyScan.groovy
@@ -1,6 +1,7 @@
-def call(String VID, String VKEY) {
+def call(String APP_PROFILE, String VID, String VKEY) {
     echo "[INFO] Veracode SAST - Policy Scan"
+    echo "[INFO] APP PROFILE NAME: ${APP_PROFILE}"
     sh "ls -al ./app/target"
 
-    veracode applicationName: 'verademo_jenkins', canFailJob: true, createSandbox: false, criticality: 'VeryHigh', deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: '', scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 20, uploadIncludesPattern: 'app/target/verademo.war', vid: "${VID}", vkey: "${VKEY}", waitForScan: true
+    veracode applicationName: "${APP_PROFILE}", createProfile: true, createSandbox: false, criticality: 'VeryHigh', canFailJob: true, deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: '', scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 20, uploadIncludesPattern: 'app/target/verademo.war', vid: "${VID}", vkey: "${VKEY}", waitForScan: true
 }
--- vars/veracodeSandboxScan.groovy
+++ vars/veracodeSandboxScan.groovy
@@ -1,6 +1,7 @@
-def call(String VID, String VKEY) {
+def call(String APP_PROFILE, String VID, String VKEY) {
     echo "[INFO] Veracode SAST - Sandbox Scan"
+    echo "[INFO] APP PROFILE NAME: ${APP_PROFILE}"
     sh "ls -al ./app/target"
 
-    veracode applicationName: 'verademo_jenkins', canFailJob: true, createSandbox: true, criticality: 'VeryHigh', deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: 'dev_branch_jenkins', scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 20, uploadIncludesPattern: 'app/target/verademo.war', vid: "${VID}", vkey: "${VKEY}", waitForScan: true
+    veracode applicationName: "${APP_PROFILE}", createProfile: true, createSandbox: true, criticality: 'VeryHigh', canFailJob: true, deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: 'dev_branch_jenkins', scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 20, uploadIncludesPattern: 'app/target/verademo.war', vid: "${VID}", vkey: "${VKEY}", waitForScan: true
 }
