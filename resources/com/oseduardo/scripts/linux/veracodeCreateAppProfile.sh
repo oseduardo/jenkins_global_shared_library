ae021f8e9e10c76283ae5cbfcd7bfc8d9fd9880b
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-08 14:24:36 -0400

    veracodeCreateAppProfile.sh

---
+++ resources/com/oseduardo/scripts/linux/veracodeCreateAppProfile.sh
@@ -0,0 +1,36 @@
+#/bin/bash
+
+#$1 API-ID
+#$2 API-Key
+#$3 AppName
+
+JAVA_WRAPPER_LOCATION="."
+
+echo '[INFO] ------------------------------------------------------------------------'
+echo '[INFO] DOWNLOADING VERACODE JAVA WRAPPER'
+if `wget --no-check-certificate https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/24.7.14.0/vosp-api-wrappers-java-24.7.14.0.jar -O VeracodeJavaAPI.jar`; then
+    chmod 755 VeracodeJavaAPI.jar
+    echo '[INFO] SUCCESSFULLY DOWNLOADED WRAPPER'
+else
+    echo '[ERROR] DOWNLOAD FAILED'
+    exit 1
+fi
+
+echo '[INFO] ------------------------------------------------------------------------'
+echo '[INFO] VERACODE UPLOAD AND SCAN'
+
+app_ID=$(java -verbose -jar $JAVA_WRAPPER_LOCATION/VeracodeJavaAPI.jar -vid $1 -vkey $2 -action GetAppList | grep -w \"$3\" | sed -n 's/.* app_id=\"\([0-9]*\)\" .*/\1/p')
+
+if [ -z "$app_ID" ];
+then
+    echo '[INFO] App does not exist'
+    echo '[INFO] create app: ' $3
+    creat_addp=$(java -jar $JAVA_WRAPPER_LOCATION/VeracodeJavaAPI.jar -vid $1 -vkey $2 -action createApp -appname "$3" -criticality high)
+    echo '[INFO]app created'
+    app_ID=$(java -jar $JAVA_WRAPPER_LOCATION/VeracodeJavaAPI.jar -vid $1 -vkey $2 -action GetAppList | grep -w \"$3\" | sed -n 's/.* app_id=\"\([0-9]*\)\" .*/\1/p')
+    echo '[INFO] new App-ID: ' $app_ID
+    echo ""
+else
+    echo '[INFO] App-IP: ' $app_ID
+    echo ""
+fi
--- vars/veracodeCreateAppProfile.groovy
+++ vars/veracodeCreateAppProfile.groovy
@@ -2,6 +2,6 @@ def call(String APP_PROFILE, String VID, String VKEY) {
     echo "[INFO] Veracode - Creating a New App Profile"
     echo "[INFO] APP PROFILE NAME: ${APP_PROFILE}"
     
-    def shellScript = libraryResource 'com/oseduardo/scripts/linux/hello-world.sh'
+    def shellScript = libraryResource 'com/oseduardo/scripts/linux/veracodeCreateAppProfile.sh'
     echo "${shellScript}"
 }
