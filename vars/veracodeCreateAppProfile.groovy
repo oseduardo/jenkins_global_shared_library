9b2da3b2d395650d92c97ba4ff4935055dcfd41c
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-08 13:40:29 -0400

    1

--- vars/veracodeCreateAppProfile.groovy
+++ vars/veracodeCreateAppProfile.groovy
@@ -1,5 +1,5 @@
 def call(String APP_PROFILE, String VID, String VKEY) {
     echo "[INFO] Veracode - Creating a New App Profile"
     echo "[INFO] APP PROFILE NAME: ${APP_PROFILE}"
-    sh "ls ../resources/com/oseduardo/scripts/linux"
+    sh "ls ./resources/com/oseduardo/scripts/linux"
 }
