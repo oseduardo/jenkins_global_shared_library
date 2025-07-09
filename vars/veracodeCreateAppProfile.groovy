1259c7fbe176d67e96fda6af4b707b895131f8fe
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-09 18:43:19 -0400

    probando verademo

--- vars/veracodeCreateAppProfile.groovy
+++ vars/veracodeCreateAppProfile.groovy
@@ -28,7 +28,7 @@ def call(String REPO_NAME, String PRODUCT_NAME, String PRODUCT_ID, String VID, S
             echo "strXML: ${strXML}"
             def parseXML = new XmlParser().parseText(strXML)
             //appID = parseXML.value.appslist.app.findAll { app -> app.@app_name == "verademo" }*.app_id
-            appID = parseXML.value
+            appID = parseXML.'*'.find { it.app.'@app_name'.text() == "verademo" }.app_id.text()
             echo "appID: ${appID}"
         }
 
