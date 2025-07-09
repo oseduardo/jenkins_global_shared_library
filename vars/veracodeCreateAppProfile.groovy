6625f5ee64cc0efaecf67f438efa78d8cef71c0b
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-09 09:49:12 -0400

    trying execute command

--- vars/veracodeCreateAppProfile.groovy
+++ vars/veracodeCreateAppProfile.groovy
@@ -14,13 +14,46 @@ def call(String REPO_NAME, String PRODUCT_NAME, String PRODUCT_ID, String VID, S
     sh "ls"
 
     //Using class ProcessBuilder 
-    def pb = new ProcessBuilder("myShellScriptFile.sh").inheritIO()
-    Map<String, String> env = pb.environment()
-    env.put( "API-ID", "${VID}" )
-    env.put( "API-Key", "${VKEY}" )
-    env.put( "AppName", "${appProfileName}" )
-    env.put( "ProductName", "${PRODUCT_NAME}" )
-    env.put( "ProductID", "${PRODUCT_ID}" )
-    Process p = pb.start()
-    p.waitFor()
+    //def pb = new ProcessBuilder("myShellScriptFile.sh").inheritIO()
+    //Map<String, String> env = pb.environment()
+    //env.put( "API-ID", "${VID}" )
+    //env.put( "API-Key", "${VKEY}" )
+    //env.put( "AppName", "${appProfileName}" )
+    //env.put( "ProductName", "${PRODUCT_NAME}" )
+    //env.put( "ProductID", "${PRODUCT_ID}" )
+    //Process p = pb.start()
+    //p.waitFor()
+
+
+
+
+    def script = "./myShellScriptFile.sh"
+    //def varAPIID = "${VID}"
+    //def varAPIKey = "${VKEY}"
+    //def varAppProfileName = "${appProfileName}"
+    //def varProductName = "${PRODUCT_NAME}"
+    //def varProductID = "${PRODUCT_ID}"
+    
+    // we have to create HashMap from HashMap here! 
+    // // Note the result of method! 
+    // // // https://docs.oracle.com/javase/7/docs/api/java/lang/System.html#getenv() 
+    // // // "Returns an unmodifiable string map view of the current system environment." 
+    myenv = new HashMap(System.getenv())
+    myenv.put("API-ID", "${VID}" ) 
+    myenv.put("API-Key", "${VKey}" ) 
+    myenv.put("AppName", "${appProfileName}" ) 
+    myenv.put("ProductName", "${PRODUCT_NAME}" ) 
+    myenv.put("ProductID", "${PRODUCT_ID}" ) 
+    // // we have to convert to array before calling execute 
+    String[] envarray = myenv.collect { k, v -> "$k=$v" }
+
+    def std_out = new StringBuilder()
+    def std_err = new StringBuilder()
+
+    proc = script.execute( envarray, null )
+
+    proc.consumeProcessOutput(std_out, std_err)
+
+    println std_out
+
 }
