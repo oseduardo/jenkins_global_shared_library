d207e861594d2e407e363e2e25ab06a57422abc2
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-09 23:11:09 -0400

    fghf

--- vars/veracodeCreateAppProfile.groovy
+++ vars/veracodeCreateAppProfile.groovy
@@ -25,11 +25,9 @@ def call(String REPO_NAME, String PRODUCT_NAME, String PRODUCT_ID, String VID, S
             strXML = "${appIDResponse}".substring(intBeginXML, intEndXML + 10)
             echo "strXML: ${strXML}"
             def parseXML = new XmlParser().parseText(strXML)
+            echo "parseXML: ${parseXML}"
             //appXMLRecord = parseXML.'*'.findAll { it.toString().indexOf("app_name=\"${appProfileName}\"") != -1 }.toString()
-            strTemp = "app_name=\"${appProfileName}\""
-            echo "strTemp: ${strTemp}"
-            appXMLRecord = parseXML.'*'.findAll { it.toString().indexOf(strTemp) != -1 }.toString()
-            //appXMLRecord = parseXML.'*'[0].toString()
+            appXMLRecord = parseXML.'*'[0].toString()
             echo "appXMLRecord: ${appXMLRecord}"
             if("${appXMLRecord}" != "[]"){
                 appID = appXMLRecord.substring("${appXMLRecord}".indexOf("app_id=") + 7,"${appXMLRecord}".indexOf(", app_name=${appProfileName},"))
