850af32cd5c40d3bc73c47f4946bb67e908fd8b2
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-10 11:21:34 -0400

    jfghj

--- vars/veracodeCreateAppProfile.groovy
+++ vars/veracodeCreateAppProfile.groovy
@@ -46,8 +46,8 @@ def call(String REPO_NAME, String PRODUCT_NAME, String PRODUCT_ID, String VID, S
                 echo "Index App Name: ${intIndexAppName}"
             }
             else{ //There's a "}" after app profile name
-                appXMLRecord = parseXML.'*'.findAll { it.toString().indexOf("app_name=${appProfileName}\}") != -1 }.toString()
-                intIndexAppName = "${appXMLRecord}".indexOf(", app_name=${appProfileName}\}")
+                appXMLRecord = parseXML.'*'.findAll { it.toString().indexOf("app_name=${appProfileName}/}") != -1 }.toString()
+                intIndexAppName = "${appXMLRecord}".indexOf(", app_name=${appProfileName}/}")
                 echo "Index App Name: ${intIndexAppName}"
             }
             /***********************************/
@@ -81,8 +81,8 @@ def call(String REPO_NAME, String PRODUCT_NAME, String PRODUCT_ID, String VID, S
                         echo "Index App Name: ${intIndexAppName}"
                     }
                     else{ //There's a "}" after app profile name
-                        appXMLRecord2 = parseXML2.'*'.findAll { it.toString().indexOf("app_name=${appProfileName}\}") != -1 }.toString()
-                        intIndexAppName = "${appXMLRecord2}".indexOf(", app_name=${appProfileName}\}")
+                        appXMLRecord2 = parseXML2.'*'.findAll { it.toString().indexOf("app_name=${appProfileName}/}") != -1 }.toString()
+                        intIndexAppName = "${appXMLRecord2}".indexOf(", app_name=${appProfileName}/}")
                         echo "Index App Name: ${intIndexAppName}"
                     }
 
