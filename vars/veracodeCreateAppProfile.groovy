5f7ced3580491cff10c209d601cce8518a39d478
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-09 21:57:46 -0400

    fgjfg

--- vars/veracodeCreateAppProfile.groovy
+++ vars/veracodeCreateAppProfile.groovy
@@ -17,9 +17,7 @@ def call(String REPO_NAME, String PRODUCT_NAME, String PRODUCT_ID, String VID, S
         echo "Java API Wrapper Version: ${wrapperVersion}"
 
         //Validate if appProfileName exists; if so, the AppID is captured
-        //appID = "java -verbose -jar ${strJavaWrapperLocation}/VeracodeJavaAPI.jar -vid ${VID} -vkey ${VKEY} -action GetAppList | grep -w \"${appProfileName}\" | sed -n 's/.* app_id=\"\([0-9]*\)\" .*/\1/p'".execute().text
         appIDResponse = "java -verbose -jar ${strJavaWrapperLocation}/VeracodeJavaAPI.jar -vid ${VID} -vkey ${VKEY} -action GetAppList".execute().text
-        //echo "appID: ${appIDResponse}"
 
         if("${appIDResponse}" != ""){
             int intBeginXML = "${appIDResponse}".indexOf("<?xml")
@@ -33,77 +31,27 @@ def call(String REPO_NAME, String PRODUCT_NAME, String PRODUCT_ID, String VID, S
                 appID = appXMLRecord.substring("${appXMLRecord}".indexOf("app_id=") + 7,"${appXMLRecord}".indexOf(", app_name=${appProfileName},"))
             }
             else{
-                echo "Es necesario crear App Profile"
+                echo "[INFO] CREATING APP PROFILE ${appProfileName}"
+                appProfileCreateResponse = "java -jar ${strJavaWrapperLocation}/VeracodeJavaAPI.jar -vid ${VID} -vkey ${VKEY} -action createApp -appname ${appProfileName} -criticality VeryHigh"
+                echo "[INFO] APP PROFILE ${appProfileName} HAS BEEN CREATED"
+
+                appIDResponse = "java -verbose -jar ${strJavaWrapperLocation}/VeracodeJavaAPI.jar -vid ${VID} -vkey ${VKEY} -action GetAppList".execute().text
+                if("${appIDResponse}" != ""){
+                    int intBeginXML = "${appIDResponse}".indexOf("<?xml")
+                    int intEndXML = "${appIDResponse}".indexOf("</applist>")
+                    strXML = "${appIDResponse}".substring(intBeginXML, intEndXML + 10)
+                    echo "strXML: ${strXML}"
+                    def parseXML = new XmlParser().parseText(strXML)
+                    appXMLRecord = parseXML.'*'.findAll { it.toString().indexOf("app_name=${appProfileName},") != -1 }.toString()
+                    echo "appXMLRecord: ${appXMLRecord}"
+                    if("${appXMLRecord}" != "[]"){
+                        appID = appXMLRecord.substring("${appXMLRecord}".indexOf("app_id=") + 7,"${appXMLRecord}".indexOf(", app_name=${appProfileName},"))
+                    }
+                }
             }
             echo "appID: ${appID}"
         }
-
     } catch(Exception ex) {
         println(ex)
     }
-
-
-
-
-
-
-    //def shellScript = libraryResource 'com/oseduardo/scripts/linux/veracodeCreateAppProfile.sh'
-    //echo "${shellScript}"
-    //sh "printf '${shellScript}' > myShellScriptFile.sh"
-    //sh "printf '#/bin/bash pwd' > testFile.sh"
-    //sh "ls"
-    //sh "cat myShellScriptFile.sh"
-    //sh "ls"
-
-    //Using class ProcessBuilder 
-    //echo "[INFO] Starting ProcessBuilder..."
-    //def pb = new ProcessBuilder("sh", "-c", "pwd").inheritIO()
-    //def pb = new ProcessBuilder("./testFile.sh").inheritIO()
-    //Map<String, String> env = pb.environment()
-    //env.put( "API-ID", "${VID}" )
-    //env.put( "API-Key", "${VKEY}" )
-    //env.put( "AppName", "${appProfileName}" )
-    //env.put( "ProductName", "${PRODUCT_NAME}" )
-    //env.put( "ProductID", "${PRODUCT_ID}" )
-    //Process p = pb.start()
-    //InputStream myInputStream = p.getInputStream()
-    //InputStreamReader myIsr = new InputStreamReader(myInputStream)
-    //char[] cbuf = new char[2048];  //read 1024 characters, increse to higher amount if necessary
-    //myIsr.read(cbuf);
-    //log.info(new String(cbuf))
-    //p.waitFor()
-    //echo "[INFO] ProcessBuilder Finished"
-
-
-
-
-    //def script = "myShellScriptFile.sh"
-    //def varAPIID = "${VID}"
-    //def varAPIKey = "${VKEY}"
-    //def varAppProfileName = "${appProfileName}"
-    //def varProductName = "${PRODUCT_NAME}"
-    //def varProductID = "${PRODUCT_ID}"
-    
-    // we have to create HashMap from HashMap here! 
-    // // Note the result of method! 
-    // // // https://docs.oracle.com/javase/7/docs/api/java/lang/System.html#getenv() 
-    // // // "Returns an unmodifiable string map view of the current system environment." 
-    //myenv = new HashMap(System.getenv())
-    //myenv.put("API-ID", "${VID}" ) 
-    //myenv.put("API-Key", "${VKey}" ) 
-    //myenv.put("AppName", "${appProfileName}" ) 
-    //myenv.put("ProductName", "${PRODUCT_NAME}" ) 
-    //myenv.put("ProductID", "${PRODUCT_ID}" ) 
-    // // we have to convert to array before calling execute 
-    //String[] envarray = myenv.collect { k, v -> "$k=$v" }
-
-    //def std_out = new StringBuilder()
-    //def std_err = new StringBuilder()
-
-    //proc = script.execute( envarray, null )
-
-    //proc.consumeProcessOutput(std_out, std_err)
-
-    //println std_out
-
 }
