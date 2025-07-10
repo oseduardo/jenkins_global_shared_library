efbce2ff416b557759b85e3326b6a9662981162c
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-10 15:15:39 -0400

    kghj

--- vars/veracodeCreateAppProfile.groovy
+++ vars/veracodeCreateAppProfile.groovy
@@ -83,7 +83,7 @@ def call(String REPO_NAME, String PRODUCT_NAME, String PRODUCT_ID, String VID, S
                         try {
                             //Updating field "product_name"
                             echo "[INFO] Updating product_name custom field in app profile ${appProfileName}. product_name: ${PRODUCT_NAME}"
-                            strCommand = "java -verbose -jar ${strJavaWrapperLocation}/VeracodeJavaAPI.jar -action updateapp -appid ${appID2} -customfieldname product_name -customfieldvalue \'" + ${PRODUCT_NAME} + "\'"
+                            strCommand = "java -verbose -jar ${strJavaWrapperLocation}/VeracodeJavaAPI.jar -action updateapp -appid ${appID2} -customfieldname product_name -customfieldvalue '" + "${PRODUCT_NAME}" + "'"
                             echo "strCommand: ${strCommand}"
                             productNameUpdateResponse = "java -verbose -jar ${strJavaWrapperLocation}/VeracodeJavaAPI.jar -action updateapp -appid ${appID2} -customfieldname product_name -customfieldvalue ${PRODUCT_NAME}".execute().text
                             echo "[INFO] Updating product_id custom field in app profile ${appProfileName}. product_id: ${PRODUCT_ID}"
