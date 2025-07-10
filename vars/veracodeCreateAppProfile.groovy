25b0d82a19d32d5f513271b743275aea52eee009
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-10 09:27:14 -0400

    fghf

--- vars/veracodeCreateAppProfile.groovy
+++ vars/veracodeCreateAppProfile.groovy
@@ -29,14 +29,25 @@ def call(String REPO_NAME, String PRODUCT_NAME, String PRODUCT_ID, String VID, S
             echo "parseXML: ${parseXML}"
 
             /******** Prueba REGEX *************/
-            def tmpText = "[{https://analysiscenter.veracode.com/schema/2.0/applist}app[attributes={app_id=488174, app_name=verademo, policy_updated_date=2025-07-08T21:05:39-04:00}; value=[]]"
+            //def tmpText = "[{https://analysiscenter.veracode.com/schema/2.0/applist}app[attributes={app_id=488174, app_name=verademo, policy_updated_date=2025-07-08T21:05:39-04:00}; value=[]]"
             //assert tmpText =~ /(.*)(app_name=verademo)(,|;)(.*)/
-            def matcher = tmpText =~ /(.*)(app_name=verademo)(,|;)(.*)/
-            echo "Matcher Size: " + "${matcher}"[1].toString()
+            //def matcher = tmpText =~ /(.*)(app_name=verademo)(,|;)(.*)/
+            //echo "Matcher Size: " + "${matcher}"[1].toString()
             //echo "Prueba REGEX - matcher: ${matcher}[0]"
             //myIndex = tmpText.indexOf(/(.*)(app_name=verademo)(,|;)(.*)/).toString()
             //echo "myIndex: ${myIndex}"
             /***********************************/
+
+            /******** Prueba 2 *************/
+            def tmpText = "[{https://analysiscenter.veracode.com/schema/2.0/applist}app[attributes={app_id=488174, app_name=verademo, policy_updated_date=2025-07-08T21:05:39-04:00}; value=[]]"
+            if(tmpText.indexOf("app_name=verademo,") != -1){ //There's a "," after app profile name
+                echo "Se debe trabajar con una coma"
+            }
+            else{ //There's a ";" after app profile name
+                echo "Se debe trabajar con un punto y coma"
+            }
+            /***********************************/
+
 
             appXMLRecord = parseXML.'*'.findAll { it.toString().indexOf("app_name=${appProfileName}") != -1 }.toString()
             echo "appXMLRecord: ${appXMLRecord}"
