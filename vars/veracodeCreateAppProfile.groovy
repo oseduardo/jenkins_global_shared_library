af279645add35626cbf5b4c06d44e3cc4e0ae4a9
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-10 08:30:43 -0400

    jfghj

--- vars/veracodeCreateAppProfile.groovy
+++ vars/veracodeCreateAppProfile.groovy
@@ -30,7 +30,7 @@ def call(String REPO_NAME, String PRODUCT_NAME, String PRODUCT_ID, String VID, S
 
             /******** Prueba REGEX *************/
             def tmpText = "{https://analysiscenter.veracode.com/schema/2.0/applist}app[attributes={app_id=488174, app_name=verademo, policy_updated_date=2025-07-08T21:05:39-04:00}; value=[]]"
-            assert tmpText =~ /(verademo)(,|;)?/
+            assert tmpText =~ /(verademopepitoperez)(,|;)?/
             //def matcher = tmpText =~ /(verademo)(,|;)?/
             //echo "Prueba REGEX - matcher: ${matcher}"
             /***********************************/
