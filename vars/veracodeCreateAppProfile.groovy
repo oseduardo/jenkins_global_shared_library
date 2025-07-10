82592202b7858b36a5a337edfdce1ea779a902fd
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-10 11:11:26 -0400

    jfghjfg

--- vars/veracodeCreateAppProfile.groovy
+++ vars/veracodeCreateAppProfile.groovy
@@ -73,7 +73,7 @@ def call(String REPO_NAME, String PRODUCT_NAME, String PRODUCT_ID, String VID, S
                     strXML2 = "${appIDResponse}".substring(intBeginXML2, intEndXML2 + 10)
                     echo "strXML2: ${strXML2}"
                     def parseXML2 = new XmlParser().parseText(strXML2)
-                    echo "parseXML: ${parseXML}"
+                    echo "parseXML2: ${parseXML2}"
 
                     if(parseXML2.'*'.findAll { it.toString().indexOf("app_name=${appProfileName},") != -1 }.toString() != "[]"){ //There's a "," after app profile name
                         appXMLRecord2 = parseXML2.'*'.findAll { it.toString().indexOf("app_name=${appProfileName},") != -1 }.toString()
