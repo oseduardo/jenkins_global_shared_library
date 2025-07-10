2efa029d8de05c2e3570f669df41da43be796e5d
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-09 21:34:29 -0400

    fdghfd

--- vars/veracodePolicyScan.groovy
+++ vars/veracodePolicyScan.groovy
@@ -3,5 +3,5 @@ def call(String APP_PROFILE, String VID, String VKEY) {
     echo "[INFO] APP PROFILE NAME: ${APP_PROFILE}"
     sh "ls -al ./app/target"
 
-    veracode applicationName: "${APP_PROFILE}", createProfile: true, createSandbox: false, criticality: 'VeryHigh', canFailJob: true, deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: '', scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 20, uploadIncludesPattern: 'app/target/verademo.war', vid: "${VID}", vkey: "${VKEY}", waitForScan: true
+    veracode applicationName: "${APP_PROFILE}", createProfile: false, createSandbox: false, criticality: 'VeryHigh', canFailJob: true, deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: '', scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 20, uploadIncludesPattern: 'app/target/verademo.war', vid: "${VID}", vkey: "${VKEY}", waitForScan: true
 }
--- vars/veracodeSandboxScan.groovy
+++ vars/veracodeSandboxScan.groovy
@@ -3,5 +3,5 @@ def call(String APP_PROFILE, String VID, String VKEY) {
     echo "[INFO] APP PROFILE NAME: ${APP_PROFILE}"
     sh "ls -al ./app/target"
 
-    veracode applicationName: "${APP_PROFILE}", createProfile: true, createSandbox: true, criticality: 'VeryHigh', canFailJob: true, deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: 'dev_branch_jenkins', scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 20, uploadIncludesPattern: 'app/target/verademo.war', vid: "${VID}", vkey: "${VKEY}", waitForScan: true
+    veracode applicationName: "${APP_PROFILE}", createProfile: false, createSandbox: true, criticality: 'VeryHigh', canFailJob: true, deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: 'dev_branch_jenkins', scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 20, uploadIncludesPattern: 'app/target/verademo.war', vid: "${VID}", vkey: "${VKEY}", waitForScan: true
 }
