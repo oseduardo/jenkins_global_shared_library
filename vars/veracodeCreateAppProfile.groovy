2ab4058834bb0b7bb9ac000f29e215ffc36f4244
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-09 21:00:33 -0400

    bdlfkgnhj;

--- vars/veracodeCreateAppProfile.groovy
+++ vars/veracodeCreateAppProfile.groovy
@@ -30,7 +30,7 @@ def call(String REPO_NAME, String PRODUCT_NAME, String PRODUCT_ID, String VID, S
             //appID = parseXML.'*'.get(3).name()
             //appID = parseXML.'*'[0].name()
             //appID = parseXML.'*'[97].toString().indexOf("app_name=verademo")
-            appID = parseXML.'*'.findAll { it.toString().indexOf("app_name=verademo") != -1 }
+            appID = parseXML.'*'.findAll { it.toString().indexOf("app_name=verademo,") != -1 }
             echo "appID: ${appID}"
         }
 
