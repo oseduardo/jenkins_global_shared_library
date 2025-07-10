225f64bcaa8a2519a3a404e563bf1a9f6576b0d7
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-10 07:57:22 -0400

    dfgh

--- vars/veracodeCreateAppProfile.groovy
+++ vars/veracodeCreateAppProfile.groovy
@@ -30,10 +30,8 @@ def call(String REPO_NAME, String PRODUCT_NAME, String PRODUCT_ID, String VID, S
 
             /******** Prueba REGEX *************/
             def tmpText = "{https://analysiscenter.veracode.com/schema/2.0/applist}app[attributes={app_id=488174, app_name=verademo, policy_updated_date=2025-07-08T21:05:39-04:00}; value=[]]"
-            def test = "parte1" + " - " + "parte2"
-            echo "${test}"
-            //def matcher = tmpText =~ 'verademo"(,;)"'
-            //echo "Prueba REGEX - matcher: ${matcher}"
+            def matcher = tmpText =~ "verademo\/(,|;)\/"
+            echo "Prueba REGEX - matcher: ${matcher}"
             /***********************************/
 
             appXMLRecord = parseXML.'*'.findAll { it.toString().indexOf("app_name=${appProfileName}") != -1 }.toString()
