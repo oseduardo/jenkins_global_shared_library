296a379e154d3d786afb6ab3ab69dc37793c8f36
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-15 15:08:47 -0400

    Invocando APIs

--- vars/holaMundo.groovy
+++ vars/holaMundo.groovy
@@ -1,3 +1,7 @@
-def call(String myMessage) {
-    echo "Hola Mundo! Mi mensaje es: ${myMessage}"    
-}
+#!/usr/bin/env groovy
+//def call() {
+    //println("Hola Mundo")
+    //println(["http", "--auth-type=veracode_hmac", "https://api.veracode.com/srcclr/v3/workspaces"].execute().text)
+    //println(["echo", "'Hola Mundo2'"].execute().text)
+    println(["http", "--version"].execute().text)
+//}
--- vars/veracodeCreateSCAWorkspace.groovy
+++ vars/veracodeCreateSCAWorkspace.groovy
@@ -1,7 +1,10 @@
-def call(String REPO_NAME, String PRODUCT_NAME, String PRODUCT_ID, String VID, String VKEY) {
+def call(String REPO_NAME, String PRODUCT_NAME, String PRODUCT_ID) {
     //This procedure is created to customize the SCA (ABS) Workspace creation process for prospect's POV
+    //Authentication is made by usinh HMAC Auth, which is covered with agent running the stage that is invoking this method
+
     def wkspName = new String("${PRODUCT_ID}_${REPO_NAME}")
 
-    //Setting up HMAC for secure authentication with SCA Agent REST APIs
-    veracodeGlobalSharedLibrary.holamundo("Espero todo salga bien!")
+    //Validate if a Workspace with the name wkspName exists
+    strWorkspaces = "http --auth-type veracode_hmac GET https://api.veracode.com/srcclr/v3/workspaces".execute().text
+    echo "${strWorkspaces}"
 }
