1714a9298d003b0ff8425240213f29a841d1f4c7
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-07-15 15:48:34 -0400

    jsonSlurper

--- vars/veracodeCreateSCAWorkspace.groovy
+++ vars/veracodeCreateSCAWorkspace.groovy
@@ -3,12 +3,13 @@ def call(String REPO_NAME, String PRODUCT_NAME, String PRODUCT_ID) {
     //Authentication is made by usinh HMAC Auth, which is covered with agent running the stage that is invoking this method
 
     def wkspName = new String("${PRODUCT_ID}_${REPO_NAME}")
+    def jsonSlurper = new JsonSlurper()
 
     //Validate if a Workspace with the name wkspName exists
     try {
         //Get workspaces list
         sh 'http --auth-type veracode_hmac GET https://api.veracode.com/srcclr/v3/workspaces > workspaces.json'
-        jsonWorkspaces = readJSON file: 'workspaces.json'
+        jsonWorkspaces = jsonSlurper.parse(new File('workspaces.json'))
         echo "${jsonWorkspaces}"
     } catch(Exception ex) {
         println(ex)
