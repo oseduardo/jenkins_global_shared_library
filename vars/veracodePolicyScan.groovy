ec9d41225119865ddf7726e1745b8729bca88e4e
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-08-08 09:44:58 -0400

    Enviando todos files en veracode-artifacts folder

--- vars/veracodePolicyScan.groovy
+++ vars/veracodePolicyScan.groovy
@@ -7,5 +7,5 @@ def call(String APP_PROFILE, String ARTIFACTS_PATH, String VID, String VKEY) {
     echo "[INFO] Getting artifacts from ${ARTIFACTS_PATH}"
     echo "[INFO] Starting Veracode Policy Scan..."
 
-    veracode applicationName: "${APP_PROFILE}", createProfile: false, createSandbox: false, criticality: 'VeryHigh', canFailJob: true, deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: '', scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 60, uploadIncludesPattern: "${ARTIFACTS_PATH}", vid: "${VID}", vkey: "${VKEY}", waitForScan: true, ScanAllNonFatalTopLevelModules: true, IncludeNewModules: true
+    veracode applicationName: "${APP_PROFILE}", createProfile: false, createSandbox: false, criticality: 'VeryHigh', canFailJob: true, deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: '', scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 60, uploadIncludesPattern: "${ARTIFACTS_PATH}/*.*", vid: "${VID}", vkey: "${VKEY}", waitForScan: true, ScanAllNonFatalTopLevelModules: true, IncludeNewModules: true
 }
