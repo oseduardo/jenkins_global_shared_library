69ae5ab961d288e05a07879e98e8cfd1b70bb4a8
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-08-09 12:34:59 -0400

    withCredentials for Policy Scan

--- vars/veracodePolicyScan.groovy
+++ vars/veracodePolicyScan.groovy
@@ -1,4 +1,4 @@
-def call(String APP_PROFILE, String VID, String VKEY) {
+def call(String APP_PROFILE) {
     // Launches a Policy Scan in Veracode platform
     // APP_PROFILE: Application profile where target sandbox is located
     echo "[INFO] Veracode SAST - Policy Scan"
@@ -6,5 +6,7 @@ def call(String APP_PROFILE, String VID, String VKEY) {
     echo "[INFO] Starting Veracode Policy Scan..."
 
     // Launches Veracode Policy Scan by using veracode plugin for Jenkins
-    veracode applicationName: "${APP_PROFILE}", createProfile: false, createSandbox: false, criticality: 'VeryHigh', canFailJob: true, deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: '', scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 60, uploadIncludesPattern: "veracode-artifacts/*.*", vid: "${VID}", vkey: "${VKEY}", waitForScan: true, ScanAllNonFatalTopLevelModules: true, IncludeNewModules: true
+    withCredentials([usernamePassword(credentialsId: 'veracode-creds', usernameVariable: 'VID', passwordVariable: 'VKEY')]){
+        veracode applicationName: "${APP_PROFILE}", createProfile: false, createSandbox: false, criticality: 'VeryHigh', canFailJob: true, deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: '', scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 60, uploadIncludesPattern: "veracode-artifacts/*.*", vid: VID, vkey: VKEY, waitForScan: true, ScanAllNonFatalTopLevelModules: true, IncludeNewModules: true
+    }
 }
