4230f3001aa3f6b2a6efe20837d2c1d7779ac11a
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-08-09 11:36:11 -0400

    Ajustand Policy/Sandbox Scans

--- vars/veracodePolicyScan.groovy
+++ vars/veracodePolicyScan.groovy
@@ -1,11 +1,10 @@
-def call(String APP_PROFILE, String ARTIFACTS_PATH, String VID, String VKEY) {
+def call(String APP_PROFILE, String VID, String VKEY) {
     // Launches a Policy Scan in Veracode platform
     // APP_PROFILE: Application profile where target sandbox is located
-    // ARTIFACTS_PATH: Folder path in pipeline agent where built artifacts are available to upload for scanning
     echo "[INFO] Veracode SAST - Policy Scan"
     echo "[INFO] APP PROFILE NAME: ${APP_PROFILE}"
-    echo "[INFO] Getting artifacts from ${ARTIFACTS_PATH}"
     echo "[INFO] Starting Veracode Policy Scan..."
 
-    veracode applicationName: "${APP_PROFILE}", createProfile: false, createSandbox: false, criticality: 'VeryHigh', canFailJob: true, deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: '', scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 60, uploadIncludesPattern: "${ARTIFACTS_PATH}/*.*", vid: "${VID}", vkey: "${VKEY}", waitForScan: true, ScanAllNonFatalTopLevelModules: true, IncludeNewModules: true
+    // Launches Veracode Policy Scan by using veracode plugin for Jenkins
+    veracode applicationName: "${APP_PROFILE}", createProfile: false, createSandbox: false, criticality: 'VeryHigh', canFailJob: true, deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: '', scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 60, uploadIncludesPattern: "veracode-artifacts/*.*", vid: "${VID}", vkey: "${VKEY}", waitForScan: true, ScanAllNonFatalTopLevelModules: true, IncludeNewModules: true
 }
--- vars/veracodeSandboxScan.groovy
+++ vars/veracodeSandboxScan.groovy
@@ -2,12 +2,10 @@ def call(String APP_PROFILE, String SANDBOX_NAME, String VID, String VKEY) {
     // Launches a Sandbox Scan in Veracode platform
     // APP_PROFILE: Application profile where target sandbox is located
     // SANDBOX_NAME: Especific sandbox where scan results will be uploaded
-    // ARTIFACTS_PATH: Folder path in pipeline agent where built artifacts are available to upload for scanning
     echo "[INFO] Veracode SAST - Sandbox Scan"
     echo "[INFO] APP PROFILE NAME: ${APP_PROFILE}"
     echo "[INFO] Starting Veracode Sandbox Scan..."
 
-    sh 'ls veracode-artifacts'
-
+    // Launch Veracode Sandbox Scan by using veracode plugin for Jenkins
     veracode applicationName: "${APP_PROFILE}", createProfile: false, createSandbox: true, criticality: 'VeryHigh', canFailJob: true, deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: "${SANDBOX_NAME}", scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 60, uploadIncludesPattern: "veracode-artifacts/*.*", vid: "${VID}", vkey: "${VKEY}", waitForScan: true, ScanAllNonFatalTopLevelModules: true, IncludeNewModules: true
 }
