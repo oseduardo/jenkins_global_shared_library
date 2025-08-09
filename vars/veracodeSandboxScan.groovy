d13de95931c4ca4b99330181fbf3150710a4d3bb
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-08-09 11:50:56 -0400

    Ajustando escritura proc Sandbox Scan

--- vars/veracodeSandboxScan.groovy
+++ vars/veracodeSandboxScan.groovy
@@ -7,6 +7,25 @@ def call(String APP_PROFILE, String SANDBOX_NAME, String VID, String VKEY) {
     echo "[INFO] Starting Veracode Sandbox Scan..."
 
     // Launch Veracode Sandbox Scan by using veracode plugin for Jenkins
-    veracode applicationName: "${APP_PROFILE}", createProfile: false, createSandbox: true, criticality: 'VeryHigh', canFailJob: true, 
-        deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: "${SANDBOX_NAME}", scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 60, uploadIncludesPattern: "veracode-artifacts/*.*", vid: "${VID}", vkey: "${VKEY}", waitForScan: true, ScanAllNonFatalTopLevelModules: true, IncludeNewModules: true
+    veracode 
+        applicationName: "${APP_PROFILE}", 
+        createProfile: false, 
+        createSandbox: true, 
+        criticality: 'VeryHigh', 
+        canFailJob: true, 
+        deleteIncompleteScanLevel: '2', 
+        fileNamePattern: '', 
+        replacementPattern: '', 
+        sandboxName: "${SANDBOX_NAME}", 
+        scanExcludesPattern: '', 
+        scanIncludesPattern: '', 
+        scanName: '${BUILD_NUMBER}', 
+        teams: '', 
+        timeout: 60, 
+        uploadIncludesPattern: "veracode-artifacts/*.*", 
+        vid: "${VID}", 
+        vkey: "${VKEY}", 
+        waitForScan: true, 
+        ScanAllNonFatalTopLevelModules: true, 
+        IncludeNewModules: true
 }
