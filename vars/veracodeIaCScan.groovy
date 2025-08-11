756c1a651f46ad7d6881dee3d12e1fe8ea0cbfc8
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-08-11 16:00:20 -0400

    xdghsd

--- vars/veracodeIaCScan.groovy
+++ vars/veracodeIaCScan.groovy
@@ -6,23 +6,23 @@ def call() {
         // Download Veracode CLI tool to run Veracode Container Security
         echo "[INFO] Downloding and installing Veracode CLI..."
         sh "curl -fsS https://tools.veracode.com/veracode-cli/install | sh"
-        sh './veracode version'
+        //sh './veracode version'
 
         // Setting up Veracode API Credentials
-        sh 'export VERACODE_API_KEY_ID=' + ${VID}
-        sh 'export VERACODE_API_KEY_SECRET=' + ${VKEY}
+        //sh 'export VERACODE_API_KEY_ID=' + ${VID}
+        //sh 'export VERACODE_API_KEY_SECRET=' + ${VKEY}
 
-        sh """
-            ./veracode scan --type directory --source . --format table --output cs_iac_results.txt
-            ls -all
-        """
+        //sh """
+        //    ./veracode scan --type directory --source . --format table --output cs_iac_results.txt
+        //    ls -all
+        //"""
         //sh 'cat cs_iac_results.txt'
 
         // Reading results file
-        File myFile = new File("cs_iac_results.txt")
+        /*File myFile = new File("cs_iac_results.txt")
         List<String> lstIaCResults = myFile.readLines()
         String strPolicyEvaluation = ""
         strPolicyEvaluation = lstIaCResults.findAll { it.substring(0, 12) == "Policy Passed" }
-        echo "Prueba: ${strPolicyEvaluation}"
+        echo "Prueba: ${strPolicyEvaluation}"*/
     }
 }
