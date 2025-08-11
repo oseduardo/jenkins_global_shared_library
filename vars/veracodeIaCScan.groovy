c4f4bee5a1e4ebc87df5946e445d82a168e55d15
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-08-11 17:05:14 -0400

    debugging

--- vars/veracodeIaCScan.groovy
+++ vars/veracodeIaCScan.groovy
@@ -11,12 +11,12 @@ def call() {
         // Setting up Veracode API Credentials
         //sh 'export VERACODE_API_KEY_ID=' + ${VID}
         //sh 'export VERACODE_API_KEY_SECRET=' + ${VKEY}
-
-        sh """
-            ./veracode scan --type directory --source . --format table --output cs_iac_results.txt
-            ls -all
-        """
-        sh 'cat cs_iac_results.txt'
+        echo "[PRUEBA] Aqui vamos"
+        //sh """
+        //    ./veracode scan --type directory --source . --format table --output cs_iac_results.txt
+        //    ls -all
+        //"""
+        //sh 'cat cs_iac_results.txt'
 
         // Reading results file
         /*File myFile = new File("cs_iac_results.txt")
