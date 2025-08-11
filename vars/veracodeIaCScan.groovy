353c489a29fcc4a4d4d4df549773e33cdda7b11c
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-08-11 16:16:00 -0400

    Obviando instalacion de Veracode CLI

--- vars/veracodeIaCScan.groovy
+++ vars/veracodeIaCScan.groovy
@@ -1,22 +1,22 @@
 def call() {
     // Executes Veracode Container Security by running Veracode CLI tool to scan IaC files on local directory (work directory) of jenkins pipeline
+    // It assumes that Veracode CLI tool is already installed in jenkins agent from which this shared library is called
     echo "[INFO] Veracode IaC Scan"
 
     withCredentials([usernamePassword(credentialsId: 'veracode-creds', usernameVariable: 'VID', passwordVariable: 'VKEY')]){
         // Download Veracode CLI tool to run Veracode Container Security
-        echo "[INFO] Downloding and installing Veracode CLI..."
-        sh "curl -fsS https://tools.veracode.com/veracode-cli/install | sh"
-        //sh './veracode version'
+        //echo "[INFO] Downloding and installing Veracode CLI..."
+        //sh "curl -fsS https://tools.veracode.com/veracode-cli/install | sh"
 
         // Setting up Veracode API Credentials
         //sh 'export VERACODE_API_KEY_ID=' + ${VID}
         //sh 'export VERACODE_API_KEY_SECRET=' + ${VKEY}
 
-        //sh """
-        //    ./veracode scan --type directory --source . --format table --output cs_iac_results.txt
-        //    ls -all
-        //"""
-        //sh 'cat cs_iac_results.txt'
+        sh """
+            ./veracode scan --type directory --source . --format table --output cs_iac_results.txt
+            ls -all
+        """
+        sh 'cat cs_iac_results.txt'
 
         // Reading results file
         /*File myFile = new File("cs_iac_results.txt")
