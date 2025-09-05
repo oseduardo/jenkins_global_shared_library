eb9a614039d42503ace737f2f664c2170470b47c
Oscar Rodriguez <oscarrodriguezarias@gmail.com>
2025-09-05 10:16:28 -0400

    try catch

--- vars/veracodeSandboxScan.groovy
+++ vars/veracodeSandboxScan.groovy
@@ -6,8 +6,15 @@ def call(String APP_PROFILE, String SANDBOX_NAME) {
     echo "[INFO] APP PROFILE NAME: ${APP_PROFILE}"
     echo "[INFO] Starting Veracode Sandbox Scan..."
 
-    // Launch Veracode Sandbox Scan by using veracode plugin for Jenkins
-    withCredentials([usernamePassword(credentialsId: 'veracode-creds', usernameVariable: 'VID', passwordVariable: 'VKEY')]){
-        veracode applicationName: "${APP_PROFILE}", createProfile: false, createSandbox: true, criticality: 'VeryHigh', canFailJob: true, deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: "${SANDBOX_NAME}", scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 60, uploadIncludesPattern: "veracode-artifacts/*.*", vid: VID, vkey: VKEY, waitForScan: true, ScanAllNonFatalTopLevelModules: true, IncludeNewModules: true
+    try {
+        // Launch Veracode Sandbox Scan by using veracode plugin for Jenkins
+        withCredentials([usernamePassword(credentialsId: 'veracode-creds', usernameVariable: 'VID', passwordVariable: 'VKEY')]){
+            veracode applicationName: "${APP_PROFILE}", createProfile: false, createSandbox: true, criticality: 'VeryHigh', canFailJob: true, deleteIncompleteScanLevel: '2', fileNamePattern: '', replacementPattern: '', sandboxName: "${SANDBOX_NAME}", scanExcludesPattern: '', scanIncludesPattern: '', scanName: '${BUILD_NUMBER}', teams: '', timeout: 60, uploadIncludesPattern: "veracode-artifacts/*.*", vid: VID, vkey: VKEY, waitForScan: true, ScanAllNonFatalTopLevelModules: true, IncludeNewModules: true
+        }
+    } catch(Exception ex) {
+        println(ex)
+        return ex
     }
+
+    return 0
 }
